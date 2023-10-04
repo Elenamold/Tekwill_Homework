60576765e5eabb94be535b6dbb522301303fc111
elena.pohilenco <pohilenkoelena@mail.ru>
2023-10-04 09:21:45 +0300

    Temperatura in Fahrenheit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.text.ParseException;
-import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
